d69d104fcbbc34b1bc1985d37d6d9a8dcde00c96
Joaquim <jocaq124@gmail.com>
2025-02-04 16:56:02 -0300

    Adicionado ao Banco de Dados

---
+++ Data/ListaTarefasContext.cs
@@ -0,0 +1,15 @@
+﻿using ListaTarefasApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaTarefasApi.Data
+{
+    public class ListaTarefasContext : DbContext
+    {
+        public ListaTarefasContext(DbContextOptions<ListaTarefasContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<TipoTarefa> TiposTarefa { get; set; }
+
+    }
+}
